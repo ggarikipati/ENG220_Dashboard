f18524dc67b3523deef23a62136749d2a6e3e8d0
Gnanitha Garikipati <garikipatignanitha2001@gmail.com>
2025-06-11 14:16:25 -0600

    Update app6.py

--- ENG220-Group-006/app6.py
+++ ENG220-Group-006/app6.py
@@ -6,7 +6,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Title
-st.title("Group-006")
+#st.title("Group-006")
 
 st.markdown("""
 ### Southern United States Air Quality Visualization
