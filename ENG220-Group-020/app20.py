9372db299ae83a4307769b1e694143e8d684402c
Gnanitha Garikipati <garikipatignanitha2001@gmail.com>
2025-06-11 00:43:34 -0600

    Update app20.py

--- ENG220-Group-020/app20.py
+++ ENG220-Group-020/app20.py
@@ -9,6 +9,13 @@ This project integrates contributions from multiple groups to analyze and visual
 
 ---
 
+### **Gun Violence Data Visualization in NM**  
+Contributor: **DT**  
+Using data from Group 12 focused on **gun violence in New Mexico**, this dashboard allows users to view statistics by **date** or **city**, choosing metrics such as *victims injured*, *suspects injured*, *killed*, or *arrested*. All visualizations are presented as **bar charts** for intuitive comparisons.  
+[Streamlit Link](https://untitled2py-2zn5bldhgddpnjthbjxykd.streamlit.app/)
+
+---
+
 ### **NO₂ Monitoring in South Valley, NM**  
 Contributor: **EC**  
 Using NO₂ (Nitrogen Dioxide) data shared by Group 12, this dashboard focuses on key columns such as *Daily Max 1-Hour Reading*, *Daily AQI*, and *Observed Count*. While initial visualizations had limitations, a refined monthly average analysis of South Valley trends was separately developed for clearer insights.  
@@ -21,9 +28,4 @@ Contributor: **GP**
 Built using air quality data from Group 8, this interactive dashboard enables users to explore AQI factors across five states: **MN**, **WI**, **MI**, **MT**, and **ND**. Users can select counties and view trends using either a **Bar Chart** or **Line Chart** to observe regional environmental differences.  
 [Streamlit Link](https://finalcodegppy-vumfdtkohha8hzlipqdypx.streamlit.app/)
 
----
-
-### **Gun Violence Data Visualization in NM**  
-Contributor: **DT**  
-Using data from Group 12 focused on **gun violence in New Mexico**, this dashboard allows users to view statistics by **date** or **city**, choosing metrics such as *victims injured*, *suspects injured*, *killed*, or *arrested*. All visualizations are presented as **bar charts** for intuitive comparisons.  
-[Streamlit Link](https://untitled2py-2zn5bldhgddpnjthbjxykd.streamlit.app/) """)
+ """)
