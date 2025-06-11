40742069fe26f3b728d6f56c659ebf418ae08d06
Gnanitha Garikipati <garikipatignanitha2001@gmail.com>
2025-06-11 00:41:39 -0600

    Update page3.py

--- ENG220-Group-020/pages/page3.py
+++ ENG220-Group-020/pages/page3.py
@@ -4,7 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os
 
-st.title("Dataset 3 – Gun Violence")
+st.title("Dataset 3 – Air Quality")
 
 # Use os.path for file compatibility in unified dashboard
 current_dir = os.path.dirname(__file__)
