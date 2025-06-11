581553af350f44d994ff1f53048c8f41d3735843
Gnanitha Garikipati <garikipatignanitha2001@gmail.com>
2025-06-11 16:28:49 -0600

    Update app5.py

--- ENG220-Group-005/app5.py
+++ ENG220-Group-005/app5.py
@@ -7,8 +7,8 @@ import matplotlib.pyplot as plt
 import os
 
 # Title and description
-st.set_page_config(page_title="Group-005", layout="wide")
-st.title("Group-005")
+
+#st.title("Group-005")
 
 st.markdown("""
 ### California Air Pollution Visualization Dashboard
