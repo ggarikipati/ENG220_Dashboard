240e0a907148a22b903d37942ec6bd9e6b6fbfcc
Gnanitha Garikipati <garikipatignanitha2001@gmail.com>
2025-06-26 11:36:45 -0600

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-# main.py
-from st_pages import Page, show_pages_from_config
-
-# Show pages from config
-show_pages_from_config(".streamlit/pages_sections.toml")
-
-# You don't need pg.run() here — the navigation will handle it.
