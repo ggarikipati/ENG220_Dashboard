6df8e988f2323f9eff351cf8894d97e73cb416ef
Gnanitha Garikipati <garikipatignanitha2001@gmail.com>
2025-07-05 15:26:48 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 from st_pages import add_page_title, get_nav_from_toml
 
 # Set wide layout and page title
-print("Creating toggle with key: use_sections_toggle")
+
 # Sidebar toggle to show grouped or flat navigation
 use_sections = st.sidebar.toggle("Group by Sections", value=True, key="use_sections_toggle_main")
 
